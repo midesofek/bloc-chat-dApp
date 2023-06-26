b1cc60b458028cbcfd02125ce4ec6975d4c0b1d3
Mide Sofek <107212205+midesofek@users.noreply.github.com>
2023-06-26 08:30:34 +0100

    update streamrChat.js
    fixed stream subscription bug

--- streamrChat.js
+++ streamrChat.js
@@ -8,7 +8,20 @@ const btnSubmit = document.getElementById("submit--button");
 const streamrClient = new StreamrClient({
   auth: { ethereum: window.ethereum },
 });
+// Get StreamId
+const streamId = "0x1339514086fc15c5e38af4e0407c469ca3911992/35153/0";
 
+// ENABLING SUBSCRIPTIONS TO STREAM
+streamrClient.subscribe(streamId, (message) => {
+  console.log(message);
+  const incMessage = message.message;
+  console.log("IncMessage is:", incMessage);
+  const messageElement = document.createElement("p");
+  messageElement.textContent = incMessage;
+  messageWindow.appendChild(messageElement);
+});
+
+// Connecting to wallet
 const connectWallet = async () => {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -27,9 +40,7 @@ const connectWallet = async () => {
   }
 };
 
-// Get StreamId
-const streamId = "0x1339514086fc15c5e38af4e0407c469ca3911992/35153/0";
-
+// publishes message to UI
 async function sendMessage() {
   try {
     const message = inputMessage.value;
@@ -39,18 +50,13 @@ async function sendMessage() {
     // ======== CREATING MESSAGES ========= //
     // Publish the message to the chat stream
     await streamrClient.publish(streamId, { message });
+    // Clear the input field
     inputMessage.value = "";
 
-    // Clear the input field
-
-    // ENABLING SUBSCRIPTIONS TO STREAM
-    console.log("Subscribing to MESSAGES..........");
-    await streamrClient.subscribe(streamId, (message) => {
-      console.log(message);
-    });
-    const messageElement = document.createElement("p");
-    messageElement.textContent = message;
-    messageWindow.appendChild(messageElement);
+    // display message on the chat window
+    // const messageElement = document.createElement("p");
+    // messageElement.textContent = message;
+    // messageWindow.appendChild(messageElement);
   } catch (error) {
     console.error(error);
   }
